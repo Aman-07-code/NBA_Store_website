3f4f32abb5d856b74d56b3694d804758749b9459
Aman-07-code <amansinghbisht6@gmail.com>
2024-06-06 23:41:15 +0530

    changes

---
+++ main.js
@@ -0,0 +1,15 @@
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () =>{
+
+  menu.classList.toggle('fa-times');
+  navbar.classList.toggle('active');
+
+}
+
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 20,
+});
